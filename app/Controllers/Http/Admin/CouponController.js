a3651aa19a1bb5ad8d0778b713d48dd258d66349
Gabriel Feitosa <gabriel.perino99@gmail.com>
2020-08-03 12:11:59 -0300

    implemented courpon controller and created services

--- app/Controllers/Http/Admin/CouponController.js
+++ app/Controllers/Http/Admin/CouponController.js
@@ -4,6 +4,9 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Coupon= use('App/Models/Coupon')
+const Database = use('Database')
+const Service = use('App/Services/Coupon/CouponService')
 /**
  * Resourceful controller for interacting with coupons
  */
@@ -15,9 +18,20 @@ class CouponController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
+   * @param {object} ctx.pagination
    */
-  async index ({ request, response, view }) {
+  async index ({ request, response, pagination }) {
+    const code= request.input('code')
+    const query = await Coupon.query()
+
+    if(code) {
+      querry.where('code','LIKE',`%${code}%`)
+    }
+
+    const coupons = await query.paginate(pagination.page,pagination.limit)
+
+    return response.send(coupons)
+
   }
 
 
@@ -30,6 +44,85 @@ class CouponController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+
+    const trx = await Database.beginTransaction()
+
+    /**
+     * Business rules for coupons: 
+     * 
+     * 1 - Products - A coupon can only be used for specific products
+     * 
+     * 2- Clients - A coupon can only be used by specific clients
+     * 
+     * 3- Products and CLients - A coupon can only be used by specific clients
+     *    and for specific products
+     * 
+     * 4- The coupon can be used by any client and for any product
+     */
+
+    var can_use_for = {
+      client:false,
+      product:false,
+    }
+
+    try {
+      const couponData = request.only([
+        'code',
+        'discount',
+        'valid_from',
+        'valid_unti',
+        'quantity',
+        'type',
+        'recursive'
+      ])
+
+      const {users,products} = request.only('users','products')
+
+      const coupon = await Coupon.create(couponData,trx)
+
+      //Service layer
+
+      const service = new Service(coupon,trx)
+
+      //Iserts the coupon relationships into de db
+
+      if(users && users.length > 0 ) {
+        await service.syncUsers(users)
+        can_use_for.client= true
+      }
+
+      if(products && products.length > 0 ) {
+        await service.syncProducts(products)
+        can_use_for.product= true
+      }
+
+      //verify who can use the coupon
+
+      if(can_use_for.product && can_use_for.client) {
+        coupon.can_use_for = 'product_client'
+      } else if (can_use_for.product && !can_use_for.client) {
+        coupon.can_use_for='product'
+      } else if (!can_use_for.product && can_use_for.client) {
+        coupon.can_use_for='client'
+      } else {
+        coupon.can_use_for='all'
+      }
+
+      //commiting the transaction
+      await coupon.save(trx)
+
+      await trx.commit()
+
+      return response.status(201).send(coupon)
+
+    } catch (error) {
+      await trx.rollback()
+
+      return response.status(400).send({
+        message: 'Could not create coupon at this time!'
+      })
+    }
+
   }
 
   /**
@@ -41,7 +134,11 @@ class CouponController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params:{id}, request, response, view }) {
+
+    const coupon = await Coupon.findOrFail(id)
+
+    return response.send(coupon)
   }
 
 
@@ -53,7 +150,73 @@ class CouponController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params:{id}, request, response }) {
+
+    const trx = await Database.beginTransaction()
+    var coupon = await  Coupon.findOrFail(id)
+
+    var can_use_for = {
+      client:false,
+      product:false,
+    }
+
+    try {
+
+      const couponData = request.only([
+        'code',
+        'discount',
+        'valid_from',
+        'valid_unti',
+        'quantity',
+        'type',
+        'recursive'
+      ])
+
+      coupon.merge(couponData)
+
+      const {users,products} = request.only(['users','products'])
+
+      const service = new Service(coupon,trx)
+
+      //Iserts the coupon relationships into de db
+
+      if(users && users.length > 0 ) {
+        await service.syncUsers(users)
+        can_use_for.client= true
+      }
+
+      if(products && products.length > 0 ) {
+        await service.syncProducts(products)
+        can_use_for.product= true
+      }
+
+      if(can_use_for.product && can_use_for.client) {
+        coupon.can_use_for = 'product_client'
+      } else if (can_use_for.product && !can_use_for.client) {
+        coupon.can_use_for='product'
+      } else if (!can_use_for.product && can_use_for.client) {
+        coupon.can_use_for='client'
+      } else {
+        coupon.can_use_for='all'
+      }
+
+      //commiting the transaction
+      await coupon.save(trx)
+
+      await trx.commit()
+
+      return response.send(coupon)
+
+
+      
+    } catch (error) {
+      await trx.rollback()
+
+      return response.status(400).send({
+        message:'Could not update coupon at this time!'
+      })
+    }
+
   }
 
   /**
@@ -64,7 +227,26 @@ class CouponController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params:{id}, request, response }) {
+
+    const trx = await Database.beginTransaction()
+    const coupon = await Coupon.findOrFail(id)
+
+    try {
+      // undoes all relationships that the coupon had with 
+      // other tables before deleting it 
+      await coupon.products().detach([],trx)
+      await coupon.orders().detach([],trx)
+      await coupon.users().detach([],trx)
+      await trx.commit()
+      return response.status(204).send()
+    } catch (error) {
+      await trx.rollback()
+      return response.status(400).send({
+        message: 'It was not possible to delete the coupon at this time!'
+      })
+    }
+
   }
 }
 
