5db552978674e3f7b68a850398aeb4182440a34a
Gabriel Feitosa <gabriel.perino99@gmail.com>
2020-08-14 15:19:28 -0300

    added apply and remove discount routes

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -23,6 +23,8 @@ Route.group(() => {
 
     Route.resource('/products','ProductController').apiOnly()
     Route.resource('/coupons','CouponController').apiOnly()
+    Route.post('/orders/:id/discount','OrderController.applyDiscount')
+    Route.delete('/orders/:id/discount', 'OrderController.removeDiscount')
     Route.resource('/orders','OderController').apiOnly()
     Route.resource('/users','UserController').apiOnly()
     Route.resource('/images','ImageController').apiOnly()
